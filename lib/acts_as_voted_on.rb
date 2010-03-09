770cbc7bf4d6d79aa0f5a0caf25247e8dac5dfee
Jeremy Gailor <jeremy@infinitecube.com>
2010-03-09 00:09:07 -0800

    jg - Basic plugin

--- lib/acts_as_voted_on.rb
+++ lib/acts_as_voted_on.rb
@@ -1,1 +1,23 @@
-# ActsAsVotedOn
+module ActiveRecord
+  module Acts
+    module VotedOn
+      def self.included(base)
+        base.extend(ClassMethods)
+      end
+      
+      module ClassMethods
+        def acts_as_voted_on
+          include ActiveRecord::Acts::VotedOn::InstanceMethods
+        end
+      end
+      
+      module InstanceMethods
+        def vote_up
+          puts "I have been voted up!"
+        end
+      end
+    end
+  end
+end
+
+ActiveRecord::Base.send(:include, ActiveRecord::Acts::VotedOn)
